3dd335095895960a671055d7522817912d7e141c
anabelmaresc <ana.belisa9403@gmail.com>
2019-02-20 05:03:58 -0500

    Add a migration of productos_table

---
+++ inventario/database/migrations/2019_02_20_095613_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->string('nombre');
+            $table->integer('referencia');
+            $table->integer('precio');
+            $table->integer('costo');
+            $table->integer('unidades');
+            $table->tinyInteger('estado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
