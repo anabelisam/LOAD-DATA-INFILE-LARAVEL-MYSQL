d60d9cc07f992df26c5280c9965fcca5f2e55b83
anabelmaresc <ana.belisa9403@gmail.com>
2019-02-20 05:03:47 -0500

    Add table to View with product list

--- inventario/resources/views/products.blade.php
+++ inventario/resources/views/products.blade.php
@@ -1,5 +1,29 @@
 <html>
     <body>
         <h1>Nueva Vista de Productos</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Referencia</th>
+                    <th>Precio</th>
+                    <th>Costo</th>
+                    <th>Unidades</th>
+                    <th>Estado</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($productos as $producto)
+                    <tr>
+                        <td>{{ $producto->nombre }}</td>
+                        <td>{{ $producto->referencia }}</td>
+                        <td>{{ $producto->precio }}</td>
+                        <td>{{ $producto->costo }}</td>
+                        <td>{{ $producto->unidades }}</td>
+                        <td>{{ $producto->estado }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </body>
 </html>
